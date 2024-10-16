cb9ee08ef166f22c50b857a4edcd66b0d5510af4
AkshayKumarMandala <akshaykumarmandala06@gmail.com>
2024-10-16 15:58:51 +0530

    Update App.tsx
    root component of application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Map from './components/Map';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Services from './pages/Services';
-import Contact from './pages/Contact';
-import Map from './components/Map'; 
-import './App.css';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="app">
+      <div className="App">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/map" element={<Map />} /> {/* Optional: Map as a page */}
         </Routes>
+        <Map />
         <Footer />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
