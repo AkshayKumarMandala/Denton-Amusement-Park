44f863992c962c10bc66d98f3827aa36933fada8
AkshayKumarMandala <akshaykumarmandala06@gmail.com>
2024-10-16 16:02:49 +0530

    Update Navbar.tsx
    basic dashboards

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,23 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/">Denton Amusement Park</Link>
-      </div>
-      <ul className="navbar-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/services">Services</Link>
-        </li>
-        <li>
-          <Link to="/map">Park Map</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/admin">Admin Dashboard</Link></li>
+        <li><Link to="/visitor">Visitor Dashboard</Link></li>
       </ul>
     </nav>
   );
