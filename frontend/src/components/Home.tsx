9e86d6ddd04dec73f1420dc0feebc3062a96bae1
AkshayKumarMandala <akshaykumarmandala06@gmail.com>
2024-10-16 16:04:00 +0530

    Create Home.tsx
    home page

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Accessible Park</h1>
+      <p>Enjoy the park with assistive technologies, accessible rides, and more!</p>
+    </div>
+  );
+};
+
+export default Home;
